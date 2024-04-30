8d41b610a147cb72e52a6caae21a36e9b12f74e7
Munseok Song <song3392@gmail.com>
2024-04-30 16:45:32 +0900

    Updated: airbnb-style eslint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import AppController from './app.controller';
+import AppService from './app.service';
+import UserModule from './user/user.module';
+import AuthModule from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export default class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export default class AppService {
+  private readonly greeting: string;
+
+  constructor() {
+    this.greeting = 'Hello World!';
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    return this.greeting;
   }
 }

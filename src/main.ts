fb16eddbc96432760cd7c43626dd47e469be0e14
Munseok Song <song3392@gmail.com>
2024-04-30 16:48:58 +0900

    Updated: corsOptions

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import AppModule from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();

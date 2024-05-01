630ca215fed98a19e409e04f017439d9eb5fd9c9
Munseok Song <song3392@gmail.com>
2024-05-01 16:27:20 +0900

    Implement: Update user schema

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -1,24 +1,52 @@
-import { Document } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
 
 export type UserDocument = User & Document;
 
-@Schema()
+@Schema({ collection: 'users' })
 export class User {
-  @Prop()
-  id: string;
-
-  @Prop()
+  @Prop({ required: true })
   email: string;
 
-  @Prop()
+  @Prop({ required: true })
   password: string;
 
-  @Prop()
-  createdAt: Date;
+  @Prop({ default: '' })
+  username: string;
+
+  @Prop({ default: 0 })
+  likes?: number;
 
   @Prop()
-  updatedAt?: Date;
+  instagram?: string;
+
+  @Prop()
+  location?: string;
+
+  @Prop()
+  intro?: string;
+
+  @Prop([{ artwork_id: { type: Types.ObjectId, ref: 'Artwork' } }])
+  liked_artworks?: { artwork_id: Types.ObjectId }[];
+
+  @Prop([{ object_id: { type: Types.ObjectId, ref: 'Artist' } }])
+  liked_artist?: { artwork_id: Types.ObjectId }[];
+
+  @Prop({ default: Date.now })
+  created_at: Date;
+
+  @Prop()
+  cover_image?: Buffer;
+
+  @Prop()
+  profile_image?: Buffer;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.pre('save', function preSave(next: () => void) {
+  if (!this.username) {
+    [this.username] = this.email.split('@');
+  }
+  next();
+});

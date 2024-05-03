86734ab2347d3211f4a2eb20b8be38bc170d7e26
Munseok Song <song3392@gmail.com>
2024-05-03 16:48:06 +0900

    Update: add ArtistModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import ArtistController from './artist/artist.controller';
+import ArtistService from './artist/artist.service';
+import ArtistModule from './artist/artist.module';
+import { ArtworkModule } from './artwork/artwork.module';
 import AppController from './app.controller';
 import AppService from './app.service';
 import UserModule from './user/user.module';
 import AuthModule from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, AuthModule, ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot(),
+    ArtistModule,
+    ArtworkModule,
+  ],
+  controllers: [AppController, ArtistController],
+  providers: [AppService, ArtistService],
 })
 export default class AppModule {}
